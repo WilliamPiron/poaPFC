f881b1bd1a5683e0d92a79f11ae2daa57434450a
wilpiron <william.piron@laposte.net>
2016-10-03 14:10:30 +0200

    Adding the two Enum avec the basic RockPaperScissors java class.

---
+++ src/fr/p10/miage/rps/model/RockPaperScissors.java
@@ -0,0 +1,14 @@
+package fr.p10.miage.rps.model;
+
+/**
+ * Created by wilpiron on 03/10/2016.
+ */
+public class RockPaperScissors {
+    public RockPaperScissors(){
+
+    }
+
+    Result play(RPSEnum p1, RPSEnum p2){
+        return Result.LOST;
+    }
+}
