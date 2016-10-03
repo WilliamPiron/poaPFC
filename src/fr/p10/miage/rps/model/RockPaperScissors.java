8f69db641c076ff65b2d50814cd69e9a37a30d7a
wilpiron <william.piron@laposte.net>
2016-10-03 14:55:37 +0200

    Adding a working method play() to RockPaperScissors.

--- src/fr/p10/miage/rps/model/RockPaperScissors.java
+++ src/fr/p10/miage/rps/model/RockPaperScissors.java
@@ -8,7 +8,15 @@ public class RockPaperScissors {
 
     }
 
-    Result play(RPSEnum p1, RPSEnum p2){
+    public Result play(RPSEnum p1, RPSEnum p2){
+        if (p1.equals(p2)) {
+            return Result.TIE;
+        }
+        if ((p1.equals(RPSEnum.PAPER) && p2.equals(RPSEnum.ROCK)) ||
+                (p1.equals(RPSEnum.ROCK) && p2.equals(RPSEnum.SCISSORS)) ||
+                (p1.equals(RPSEnum.SCISSORS) && p2.equals(RPSEnum.PAPER))) {
+            return Result.WIN;
+        }
         return Result.LOST;
     }
 }
