d41447fa7e40b3cfa8ced079de052a4d46d9129c
wilpiron <william.piron@laposte.net>
2016-10-03 14:43:32 +0200

    Add the first batch of tests for RockPaperScissors : - the test throws an error => THAT'S NORMAL ! play() returns Result.LOST by default

---
+++ tests/fr/p10/miage/rps/tests/RockPaperScissorsTest.java
@@ -0,0 +1,41 @@
+package fr.p10.miage.rps.tests;
+
+import fr.p10.miage.rps.model.RPSEnum;
+import fr.p10.miage.rps.model.Result;
+import fr.p10.miage.rps.model.RockPaperScissors;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by wilpiron on 03/10/2016.
+ */
+public class RockPaperScissorsTest {
+
+    RockPaperScissors rps;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        rps = new RockPaperScissors();
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        rps = null;
+    }
+
+    @Test
+    public void testPlay() throws Exception {
+
+    }
+
+    @Parameters({"paper","rock"})
+    @Test
+    public void testWinPlay(String p1, String p2){
+        assertEquals(rps.play(RPSEnum.valueOf(p1),RPSEnum.valueOf(p2)), Result.LOST);
+    }
+
+}
